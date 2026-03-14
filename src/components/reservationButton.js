8d94af7d6a39db77efed7e2d3597ac500932057b
Calvin <khaliff.williamss@gmail.com>
2026-03-14 00:27:48 -0700

    added stylistic set

--- src/components/reservationButton.js
+++ src/components/reservationButton.js
@@ -1,3 +1,3 @@
 export default function ReservationButton({ data }) {
-  return <a className="reservation-button button-primary m-hide" href={data?.reservation?.link}>{data?.reservation?.button}</a>;
+  return <a className="reservation-button button-secondary m-hide" href={data?.reservation?.link}>{data?.reservation?.button}</a>;
 }
